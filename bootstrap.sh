2e9dec197d1c24e9fecb2e192dfd98283436f8c0
matthewbeta <hello@matthew-jackson.com>
2021-01-20 09:00:51 +0000

    trying to fix not foound error

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,13 +15,10 @@ function doIt() {
 	source ~/.bash_profile;
 }
 
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
+
+read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+echo "";
+if [[ $REPLY =~ ^[Yy]$ ]]; then
 	doIt;
-else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-	echo "";
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt;
-	fi;
 fi;
 unset doIt;
